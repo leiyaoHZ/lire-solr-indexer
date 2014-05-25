61f38a2ebe7cc68acba33b8fa1bd526b23b3fc35
Nurul Ferdous <nurul@ferdo.us>
2014-05-26 03:26:47 +0600

    fix

--- scripts/lsr.py
+++ scripts/lsr.py
@@ -2,10 +2,10 @@
 
 import os
 
-def run(folder_name="/Users/ferdous/code/java/liresolrindexer/data/images/"):
-    out_file = open('./dir.txt', 'wb')
+def run(folder_name="./data/images/"):
+    out_file = open('./out.txt', 'wb')
     for item in os.listdir(folder_name):
-        if not item.endswith("xml"):
+        if item.endswith("jpg"):
             line = folder_name + item + '\n'
             print line
             out_file.write(line)
